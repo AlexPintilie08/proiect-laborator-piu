0a908ab8e3bef0123221756ea854bab266bfc8f1
Alex <pintilie574@gmail.com>
2026-05-12 21:05:49 +0300

    tema lab6

---
+++ Transport.App/Transporturi/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using Transporturi.Entitati;
+
+namespace Transporturi.WPF
+{
+    public partial class MainWindow : Window
+    {
+        private Sofer sofer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sofer = new Sofer
+            {
+                Id = 1,
+                Nume = "Popescu Ion",
+                CategoriiPermis = "B, C, E",
+                Kilometri = 120000
+            };
+
+            AfiseazaSofer();
+        }
+
+        private void AfiseazaSofer()
+        {
+            TxtId.Text = $"ID: {sofer.Id}";
+            TxtNume.Text = $"Nume: {sofer.Nume}";
+            TxtCategorii.Text = $"Categorii permis: {sofer.CategoriiPermis}";
+            TxtKilometri.Text = $"Kilometri: {sofer.Kilometri}";
+        }
+
+        private void BtnActualizeaza_Click(object sender, RoutedEventArgs e)
+        {
+            sofer.Kilometri += 1000;
+            AfiseazaSofer();
+        }
+    }
+}
