c4fba9a6465f440c24f6114e637f0c778ea76c27
Alex <pintilie574@gmail.com>
2026-05-12 21:18:29 +0300

    tema lab8

--- Transport.App/Transporturi/MainWindow.xaml.cs
+++ Transport.App/Transporturi/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Media;
 using Transporturi.Entitati;
 
@@ -6,6 +8,8 @@ namespace Transporturi
 {
     public partial class MainWindow : Window
     {
+        private List<Sofer> soferi = new List<Sofer>();
+
         private const int KM_MIN = 0;
         private const int KM_MAX = 1000000;
 
@@ -19,6 +23,7 @@ namespace Transporturi
             bool valid = true;
 
             txtNume.Background = Brushes.White;
+            txtTelefon.Background = Brushes.White;
             txtCategorii.Background = Brushes.White;
             txtKilometri.Background = Brushes.White;
 
@@ -30,17 +35,17 @@ namespace Transporturi
                 valid = false;
             }
 
-            if (string.IsNullOrWhiteSpace(txtCategorii.Text))
+            if (string.IsNullOrWhiteSpace(txtTelefon.Text))
             {
-                txtCategorii.Background = Brushes.LightPink;
+                txtTelefon.Background = Brushes.LightPink;
                 valid = false;
             }
 
-            int kilometri;
+            int km;
 
-            if (!int.TryParse(txtKilometri.Text, out kilometri) ||
-                kilometri < KM_MIN ||
-                kilometri > KM_MAX)
+            if (!int.TryParse(txtKilometri.Text, out km) ||
+                km < KM_MIN ||
+                km > KM_MAX)
             {
                 txtKilometri.Background = Brushes.LightPink;
                 valid = false;
@@ -53,16 +58,55 @@ namespace Transporturi
                 return;
             }
 
+            string tipAngajat = rbIntern.IsChecked == true
+                ? "Intern"
+                : "Extern";
+
+            string beneficii = "";
+
+            if (cbBonus.IsChecked == true)
+                beneficii += "Bonus ";
+
+            if (cbAsigurare.IsChecked == true)
+                beneficii += "Asigurare";
+
             Sofer s = new Sofer
             {
-                Id = 1,
+                Id = soferi.Count + 1,
                 Nume = txtNume.Text,
                 CategoriiPermis = txtCategorii.Text,
-                Kilometri = kilometri
+                Kilometri = km
             };
 
+            soferi.Add(s);
+
+            lstSoferi.Items.Add(
+                $"{s.Id} | {s.Nume} | {tipAngajat} | {beneficii}");
+
             txtMesaj.Foreground = Brushes.Green;
-            txtMesaj.Text = $"Șofer adăugat: {s.Nume}";
+            txtMesaj.Text = "Șofer adăugat!";
+        }
+
+        private void BtnCauta_Click(object sender, RoutedEventArgs e)
+        {
+            string cautare = txtCautare.Text.ToLower();
+
+            var rezultate = soferi
+                .Where(s => s.Nume.ToLower().Contains(cautare))
+                .ToList();
+
+            lstSoferi.Items.Clear();
+
+            foreach (var s in rezultate)
+            {
+                lstSoferi.Items.Add(
+                    $"{s.Id} | {s.Nume} | {s.CategoriiPermis}");
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
